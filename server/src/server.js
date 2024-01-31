d7ac9895237de9a3f68701494281cf323798e9b5
0xdf00ff@gmail.com <0xdf00ff@gmail.com>
2024-01-31 08:44:00 +0500

    user auth

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const userRoutes = require('./api/routes/userRoutes');
+// const userRoutes = require('./api/routes/userRoutes');
 require('./api/config/db'); 
+
+const authRoutes = require('./api/routes/auth');
+const userRoutes = require('./api/routes/user');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(express.json());
 
-app.use('/api/users', userRoutes);
+// app.use('/api/users', userRoutes);
+
+
+app.use('/auth', authRoutes);
+app.use('/user', userRoutes);
+
 
 const HOST = process.env.HOST;
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Server has been started on http://${HOST}:${PORT}`);
